01af73bbf4c0d11c738fb4846493d71798d24d40
Olivier Beaudoin <me@olivierbeaudoin.ca>
2024-01-08 23:29:48 -0500

    Set identity permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,13 @@ REMOTE_PORT="${REMOTE_PORT:-0}"
 REMOTE_HOST="${REMOTE_HOST:-0}"
 IDENTITY_FILE="${IDENTITY_FILE:-0}"
 
+cp $IDENTITY_FILE /auth-key
+chmod 600 /auth-key
+
 CMD=(
 /usr/bin/autossh
 -M 0 -T -N -g -v
--i $IDENTITY_FILE
+-i /auth-key
 -oStrictHostKeyChecking=accept-new
 -oServerAliveInterval=180
 -R $LOCAL_PORT:localhost:$REMOTE_PORT $REMOTE_HOST
